9c1db73ed70349fbc089995938a26851f44b6884
Daniel.Khron <danielherrera@Daniels-Mac-mini.fritz.box>
2023-02-21 17:40:32 +0100

    modified some aspects to work with ownership and imports

--- contracts/KhronusNodeBaseV01.sol
+++ contracts/KhronusNodeBaseV01.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../interfaces/KhronusCoordinatorInterface.sol";
+import {KhronusCoordinatorInterface} from "../interfaces/KhronusCoordinatorInterface.sol";
 
 abstract contract KhronusNode {
     event RequestReceived(
